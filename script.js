4402ddfb5f682c9ea3c2b819cb68b6433db7fdf3
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-10-29 10:29:34 -0300

    feat: adiciona tarefas em uma lista não ordenada

---
+++ script.js
@@ -0,0 +1,24 @@
+function adicionarTarefa() {
+    let mensagem = "Tarefa adicionada com sucesso!";
+
+    //Obtendo o input da tarefa
+    let inputTarefa = document.getElementById("inputTarefa");
+    
+    /*Pra ver o inputTarefa no console é só usar o console.log(inputTarefa)
+    Esse comando retornará toda a tag do input, porém com o .value a gente pega apenas o valor que o usuário digitou nela.*/
+    
+    //Obtendo o valor da tarefa
+    let tarefa = inputTarefa.value;
+    document.getElementById("mensagem").textContent = mensagem;
+
+    let listaTarefas = document.getElementById("listaTarefas");
+    let novaTarefa = document.createElement("li");
+    
+    //Inserindo informações da tarefa dentro da li
+    novaTarefa.textContent = tarefa;
+    //A ul está dentro de listaTarefas, então com o comando append, nós adicionamos a li dentro da ul
+    listaTarefas.appendChild(novaTarefa);
+
+    //Resetando o campo da tarefa quando se dá a entrada
+    inputTarefa.value = "";
+}
