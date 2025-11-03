3f9620635baa1c1771dcc7ab6d72e8f7ba205028
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-11-03 13:01:05 -0300

    feat: tratamento de entredas vazias e removendo espaços em branco desnecessários

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 function adicionarTarefa() {
     let mensagem = "Tarefa adicionada com sucesso!";
-
+    
     //Obtendo o input da tarefa
     let inputTarefa = document.getElementById("inputTarefa");
+    
     
     /*Pra ver o inputTarefa no console é só usar o console.log(inputTarefa)
     Esse comando retornará toda a tag do input, porém com o .value a gente pega apenas o valor que o usuário digitou nela*/
     
-    //Obtendo o valor da tarefa
-    let tarefa = inputTarefa.value;
-    document.getElementById("mensagem").textContent = mensagem;
+    //Obtendo o valor da tarefa sem espaços em branco no início e no final
+    let tarefa = inputTarefa.value.trim()
 
-    let listaTarefas = document.getElementById("listaTarefas");
-    let novaTarefa = document.createElement("li");
-    
-    //Inserindo informações da tarefa dentro da li
-    novaTarefa.textContent = tarefa;
-    //A ul está dentro de listaTarefas, então com o comando append, nós adicionamos a li dentro da ul
-    listaTarefas.appendChild(novaTarefa);
-
-    //Resetando o campo da tarefa quando se dá a entrada
-    inputTarefa.value = "";
+    //tratamento de input vazio
+    if (tarefa == ""){
+        let mensagemErro = "Por favor, insira uma tarefa válida.";
+        document.getElementById("mensagem").textContent = mensagemErro; 
+    }else{ 
+        document.getElementById("mensagem").textContent = mensagem;
+        
+        let listaTarefas = document.getElementById("listaTarefas");
+        let novaTarefa = document.createElement("li");
+        
+        //Inserindo informações da tarefa dentro da li
+        novaTarefa.textContent = tarefa;
+        //A ul está dentro de listaTarefas, então com o comando append, nós adicionamos a li dentro da ul
+        listaTarefas.appendChild(novaTarefa);
+        
+        //Resetando o campo da tarefa quando se dá a entrada
+        inputTarefa.value = "";
+    }       
 }
